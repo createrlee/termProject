2ab8c431ac84b03e3ef6bb4ca1123a3f765e4e2e
createrlee <createrlee@192.168.0.10>
2015-04-13 15:40:44 +0900

    4.4(2nd) major structure change

--- PhoneBook/src/PhoneBook.java
+++ PhoneBook/src/PhoneBook.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  */
 public class PhoneBook
 {
-	private int GroupIndex;//특정 그룹을 선택해서 작업을 수행할때 이용
+	private int GroupIndex=-1;//특정 그룹을 선택해서 작업을 수행할때 이용. -1이 아닌 경우에는 선택된 그룹이 있는 것으로 간주하여 그 그룹에 대해서만 메소드 실행.
 	private ArrayList<Group> phoneBook;
 	
 	public PhoneBook()
@@ -20,45 +20,65 @@ public class PhoneBook
 		phoneBook.add(new Group("company"));
 	}
 	
+	/**전체 연락처 목록을 출력합니다*/
 	public void list()
 	{
 		
 	}
 	
+	/**전체 그룹 목록을 출력합니다*/
 	public void listGroup()
 	{
 		
 	}
 	
+	/**새로운 연락처를 추가합니다*/
 	public void add(String name,String phoneNum,String group)
 	{
 		
 	}
 	
+	/**새로운 그룹을 추가합니다*/
 	public void addGroup(String groupName)
 	{
 		
 	}
 	
+	/**그룹의 이름을 바꿉니다*/
+	public void groupChange(String groupName,String toChange)
+	{
+		
+	}
+	
+	/**연락처를 삭제합니다*/
 	public void delete(String name)	
 	{
 		
 	}
 	
-	/**연락처를 찾아 정보를 변경합니다
-	 * 
-	 * @param select :1.이름 변경 2.전화번호 변경
-	 */
-	public void change(String name,int select)
+	/**연락처를 검색하여 연락처의 정보를 모두 표시합니다*/
+	public void search(String name)
 	{
 		
 	}
 	
+	/**연락처를 찾아 정보를 변경합니다
+	 * @param name : 찾을 연락처
+	 * @param toChange : 바꿀 이름 혹은 전화번호
+	 * @param select : 1.이름 변경 2.전화번호 변경 3.그룹 변경
+	 */
+	public void change(String name,String toChange,int select)
+	{
+		
+	}
+	
+	/**선택된 연락처의 메세지 대화창을 띄웁니다*/
 	public void message(String name)
 	{
 		
 	}
 	
+	/**선택된 연락처의 전화 창을 띄웁니다*/
 	public void call(String name)
 	{
 		
--- PhoneBook/src/PhoneBookMain.java
+++ PhoneBook/src/PhoneBookMain.java
@@ -13,11 +13,12 @@ public class PhoneBookMain
 	static String name = null;
 	static String phoneNum = null;
 	static String group = null;
+	static String temp = null;
 	
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		PhoneBook phoneBook=new PhoneBook();
+		PhoneBook phoneBook = new PhoneBook();
 		
 		/*console application을 이용하기 위해 몇가지 변수를 정의*/
 		ConsoleApplication app = new ConsoleApplication();
@@ -26,44 +27,60 @@ public class PhoneBookMain
 		
 		/*state 0: 메인메뉴*/
 		newDef = new ApplicationStateDefinition(0);
-		newDef.menuText = "-----------------------------\n"
+		newDef.menuText = "-----------메인 메뉴-----------\n"
 				+ "     원하는 작업을 선택하세요\n"
 				+ "1.add 2.search 3.group 4.quit\n"
-				+ "-----------------------------\n"
+				+ "------------------------------\n"
 				+ "선택->";
 		
-		// 1 -> state 1: 연락처 추가
+		// 1 -> 연락처 추가
 		newRule = new Rule();
 		newRule.Match = input -> input.startsWith("1");
-		newRule.Execute = tokens -> 
+		newRule.Execute = tokens ->
 		{
-			System.out.print("이름을 입력하세요\n->");
+			System.out.print("*이름을 입력하세요->");
 			name = sc.nextLine();
-			System.out.print("전화번호를 입력하세요\n->");
+			System.out.print("*전화번호를 입력하세요->");
 			phoneNum = sc.nextLine();
-			System.out.println("저장할 그룹을 입력하세요. 입력한 그룹이 목록에 없는 경우 새로 생성합니다.");
+			System.out.println("*저장할 그룹을 입력하세요. 입력한 그룹이 목록에 없는 경우 새로 생성합니다.");
 			System.out.println("아무 그룹을 입력하지 않은 경우 그룹 없이 저장됩니다.");
-			phoneBook.listGroup();
+			phoneBook.listGroup();//그룹 목록 출력
 			System.out.print("->");
-			group=sc.nextLine();
+			group = sc.nextLine();
 			phoneBook.add(name, phoneNum, group);//연락처 추가
 		};
 		newDef.rules.add(newRule);
 		
-		// 2 -> 연락처 검색을 실행 후 state 2로 이동
+		// 2 -> 검색할 연락처 이름을 입력받은 후 state 1(선택된 연락처에 대한 작업선택 메뉴)으로 이동
 		newRule = new Rule();
 		newRule.Match = input -> input.startsWith("2");
-		newRule.Execute = tokens -> 
+		newRule.Execute = tokens ->
 		{
-			
+			System.out.println("검색할 연락처의 이름을 입력하세요");
+			name = sc.nextLine();
+			phoneBook.search(name);
+			app.state = 1;
+		};
+		newDef.rules.add(newRule);
+		
+		// 3 -> 그룹 목록을 출력한 후 state 2(그룹 관리 메뉴)로 이동
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("3");
+		newRule.Execute = tokens ->
+		{
+			phoneBook.listGroup();
 			app.state = 2;
 		};
 		newDef.rules.add(newRule);
 		
-		// 3 -> state 3로 이동
+		// 4 -> 종료
 		newRule = new Rule();
-		newRule.Match = input -> input.startsWith("3");
-		newRule.Execute = tokens -> app.state = 3;
+		newRule.Match = input -> input.startsWith("4");
+		newRule.Execute = tokens ->
+		{
+			System.out.println("***프로그램을 종료합니다***");
+			System.exit(0);
+		};
 		newDef.rules.add(newRule);
 		
 		// 맨 마지막에 오류 규칙 추가
@@ -72,10 +89,186 @@ public class PhoneBookMain
 		// 응용 프로그램에 지금 만든 state 정의 추가
 		app.stateDefinitions.add(newDef);
 		
+		/*state 1: 선택된 연락처에 대한 작업선택 메뉴*/
+		newDef = new ApplicationStateDefinition(1);
+		newDef.menuText = "-----------------------------------------\n"
+				+ "다음 중 검색된 전화번호에 대해 수행할 작업을 선택하세요\n"
+				+ "1.change 2.delete 3.message 4.call\n"
+				+ "5.return to main menu 6.quit\n"
+				+ "-----------------------------------------\n"
+				+ "->";
 		
+		// 1 -> state 11로 이동
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("1");
+		newRule.Execute = tokens -> app.state = 11;
+		newDef.rules.add(newRule);
 		
+		// 2 -> 선택된 연락처를 삭제한 후 state 0(메인메뉴)로 돌아간다
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("2");
+		newRule.Execute = tokens ->
+		{
+			phoneBook.delete(name);
+			System.out.println(name + "의 연락처가 삭제되었습니다");
+			app.state = 0;
+		};
+		newDef.rules.add(newRule);
 		
+		// 3 -> 선택된 연락처의 메세지 메소드 실행
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("3");
+		newRule.Execute = tokens -> phoneBook.message(name);
+		newDef.rules.add(newRule);
 		
+		// 4 -> 선택된 연락처의 전화 메소드 실행
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("4");
+		newRule.Execute = tokens -> phoneBook.call(name);
+		newDef.rules.add(newRule);
+		
+		// 5 -> state 0(메인메뉴)로 이동
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("5");
+		newRule.Execute = tokens -> app.state = 0;
+		newDef.rules.add(newRule);
+		
+		// 6 -> 프로그램 종료
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("6");
+		newRule.Execute = tokens ->
+		{
+			System.out.println("***프로그램을 종료합니다***");
+			System.exit(0);
+		};
+		newDef.rules.add(newRule);
+		
+		// 맨 마지막에 오류 규칙 추가
+		newDef.rules.add(Parser.CommonRules.rule_error);
+		
+		// 응용 프로그램에 지금 만든 state 정의 추가
+		app.stateDefinitions.add(newDef);
+		
+		/*state 2: 그룹 관리 메뉴*/
+		newDef = new ApplicationStateDefinition(2);
+		newDef.menuText = "-------------그룹 관리--------------\n"
+				+ "다음 중 수행할 작업을 선택하세요\n"
+				+ "1.list group 2.add group\n"
+				+ "3.change group name 4.delete group\n"
+				+ "5.return to main menu 6. quit\n"
+				+ "------------------------------------\n"
+				+ "->";
+		
+		// 1 -> 그룹 목록을 출력하고 특정 그룹을 선택한 후 state 21(그룹별 연락처 관리 메뉴)로 이동
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("1");
+		newRule.Execute = tokens ->
+		{
+			phoneBook.listGroup();
+			System.out.print("*관리할 그룹을 입력하세요\n->");
+			group=sc.nextLine();
+			app.state=21;
+		};
+		newDef.rules.add(newRule);
+		
+		// 2 -> 새로운 그룹을 추가
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("2");
+		newRule.Execute = tokens ->
+		{
+			System.out.print("*새롭게 추가하려는 그룹의 이름을 입력하세요\n->");
+			group=sc.nextLine();
+			phoneBook.addGroup(group);
+		};
+		newDef.rules.add(newRule);
+		
+		// 3 -> 그룹 이름 변경
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("3");
+		newRule.Execute = tokens ->
+		{
+			System.out.println("***프로그램을 종료합니다***");
+			System.exit(0);
+		};
+		newDef.rules.add(newRule);
+		
+		// 4 -> 프로그램 종료
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("4");
+		newRule.Execute = tokens ->
+		{
+			System.out.println("***프로그램을 종료합니다***");
+			System.exit(0);
+		};
+		newDef.rules.add(newRule);
+		
+		// 5 -> 프로그램 종료
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("5");
+		newRule.Execute = tokens ->
+		{
+			System.out.println("***프로그램을 종료합니다***");
+			System.exit(0);
+		};
+		newDef.rules.add(newRule);
+		
+		// 6 -> 프로그램 종료
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("6");
+		newRule.Execute = tokens ->
+		{
+			System.out.println("***프로그램을 종료합니다***");
+			System.exit(0);
+		};
+		newDef.rules.add(newRule);
+		
+		// 맨 마지막에 오류 규칙 추가
+		newDef.rules.add(Parser.CommonRules.rule_error);
+		
+		// 응용 프로그램에 지금 만든 state 정의 추가
+		app.stateDefinitions.add(newDef);
+		
+		/*state 11 : 어떤 정보를 변경할 것인지를 선택하는 메뉴*/
+		newDef = new ApplicationStateDefinition(11);
+		newDef.menuText = "----------------------------\n"
+				+ "   어떤 정보를 변경하시겠습니까?\n"
+				+ "1.name 2.phone number 3.group\n"
+				+ "-----------------------------\n"
+				+ "->";
+		
+		// 1 -> 이름을 변경한 후 state 1(검색한 연락처에 대한 작업선택 메뉴)로 이동
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("1");
+		newRule.Execute = tokens ->
+		{
+			System.out.print("변경할 이름을 입력하세요\n->");
+			temp = sc.nextLine();
+			phoneBook.change(name, temp, 1);
+			name = temp;//이름을 바꾸었으므로
+			app.state = 1;
+		};
+		newDef.rules.add(newRule);
+		
+		// 2 -> 전화번호를 변경한 후 state 1(검색한 연락처에 대한 작업선택 메뉴)로 이동
+		newRule = new Rule();
+		newRule.Match = input -> input.startsWith("2");
+		newRule.Execute = tokens ->
+		{
+			System.out.print("변경할 전화번호를 입력하세요\n->");
+			temp = sc.nextLine();
+			phoneBook.change(name, temp, 2);
+			phoneNum = temp;//전화번호를 바꾸었으므로
+			app.state = 1;
+		};
+		newDef.rules.add(newRule);
+		
+		// 맨 마지막에 오류 규칙 추가
+		newDef.rules.add(Parser.CommonRules.rule_error);
+		
+		// 응용 프로그램에 지금 만든 state 정의 추가
+		app.stateDefinitions.add(newDef);
+		
+		/*프로그램 시작*/
 		app.Start();
 		
 		/*
